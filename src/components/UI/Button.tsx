db10b20c2482e59661182040007a56223cbe5466
razvanr91 <razvan.roman91@outlook.com>
2021-12-14 14:50:02 +0000

    Add state

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -5,11 +5,12 @@ import classes from "./Button.module.css";
 interface Props {
 	children: React.ReactNode;
 	type: undefined | "button" | "submit";
+	onClick: (e: React.SyntheticEvent) => void;
 }
 
-const Button = ({ type, children }: Props) => {
+const Button = ({ type, children, onClick }: Props) => {
 	return (
-		<button className={classes.button} type={type}>
+		<button className={classes.button} type={type} onClick={onClick}>
 			{children}
 		</button>
 	);
--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
@@ -6,18 +6,31 @@ import classes from "./AddUser.module.css";
 interface Props {}
 
 const AddUser = () => {
+	const [enteredUsername, setEnteredUsername] = useState("");
+	const [enteredAge, setEnteredAge] = useState(Number);
 	const addUserHandler = (e: React.SyntheticEvent) => {
 		e.preventDefault();
+		console.log(enteredUsername, enteredAge);
+	};
+
+	const usernameChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setEnteredUsername(e.target.value);
+	};
+
+	const ageChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setEnteredAge(parseInt(e.target.value));
 	};
 
 	return (
 		<Card className={classes.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input id="username" type="text" />
+				<input id="username" type="text" onChange={usernameChangeHandler} />
 				<label htmlFor="age">Age (Years)</label>
-				<input id="age" type="number" />
-				<Button type="submit">Add User</Button>
+				<input id="age" type="number" onChange={ageChangeHandler} />
+				<Button type="submit" onClick={addUserHandler}>
+					Add User
+				</Button>
 			</form>
 		</Card>
 	);
